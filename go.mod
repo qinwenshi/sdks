608c86d31e88a296532a14c0aafc10416d643649
Qinwen Shi <qinwen.shi@gmail.com>
2020-03-01 23:54:22 +0800

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module g2rail.com/sdks
+
+go 1.13
+
+require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/golang/protobuf v1.3.4
+	github.com/grpc-ecosystem/grpc-gateway v1.13.0
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
+	google.golang.org/genproto v0.0.0-20200227132054-3f1135a288c9
+	google.golang.org/grpc v1.27.1
+)
---
+++ regen.sh
@@ -0,0 +1,14 @@
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:./generated ./online_solutions.proto
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --go_out=plugins=grpc:./generated ./online_solutions.proto
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --ruby_out=./generated ./online_solutions.proto
